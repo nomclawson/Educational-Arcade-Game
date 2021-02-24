85ec989b67ab8e9508961a4efd51cbf91496be47
dang-mang <collinmuurphy@gmail.com>
2021-02-24 12:17:14 -0700

    added game_with_server.py and client.py to send high scores to the server

---
+++ client.py
@@ -0,0 +1,25 @@
+import socket
+import threading
+import os
+
+#global variables
+HEADER = 64
+PORT = 8080
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = '!D'
+SERVER = "dangmang.xyz"
+ADDR = (SERVER, PORT)
+
+def send(msg):
+	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	print(f"Sending score to {SERVER}...")
+	client.connect(ADDR)
+	print(f"Score of {msg} sent to {SERVER} successfuly.")
+
+	msg = "\n" + msg
+	message = msg.encode(FORMAT)
+	msg_length = len(message)
+	send_length = str(msg_length).encode(FORMAT)
+	send_length += b' ' * (HEADER - len(send_length))
+	client.send(send_length)
+	client.send(message)
