61a972c64a8f5f28e49934f7186524f8d192dde2
dang-mang <collinmuurphy@gmail.com>
2021-02-24 15:24:31 -0700

    better formatting

--- client.py
+++ client.py
@@ -14,12 +14,12 @@ def send(msg):
 	client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	print(f"Sending score to {SERVER}...")
 	client.connect(ADDR)
-	print(f"Score of {msg} sent to {SERVER} successfuly.")
 
-	msg = "\n" + msg
+	msg = "\n" + msg + "<br>"
 	message = msg.encode(FORMAT)
 	msg_length = len(message)
 	send_length = str(msg_length).encode(FORMAT)
 	send_length += b' ' * (HEADER - len(send_length))
 	client.send(send_length)
 	client.send(message)
+	print(f"Score of {msg} sent to {SERVER} successfuly.")
